38b21ee15131d88971ab61a234d851cf84f5ddb2
Brand <you@example.com>
2026-07-10 12:26:22 -0400

    Added ball using 2d meshes, physics/colliders to ball and player block

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use bevy::prelude::*;
 use bevy::math::prelude::*;
+use bevy::time::Time;
 use bevy::window::{PresentMode, Window, WindowMode};
+use bevy::mesh::CircleMeshBuilder;
+use bevy_rapier2d::prelude::*;
 
 const BACKGROUND_COLOR: Color = Color::srgb(0.25, 0.25, 0.25);
 fn main() {
@@ -17,7 +20,10 @@ fn main() {
             })
             .set(ImagePlugin::default_nearest()),
         )
-        .add_systems(Startup, setup)
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
+        .add_plugins(RapierDebugRenderPlugin::default())
+        .add_systems(Startup, (setup, setup_graphics))
+        .add_systems(Update, (move_player, print_ball_altitude))
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .run();
 }
@@ -27,15 +33,64 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    commands.spawn(Camera2d);
 
-    let rect_mesh = meshes.add(Rectangle::new(125.0, 25.0));
+    let rect_mesh = meshes.add(Rectangle::new(120.0, 24.0));
 
     let rect_material = materials.add(Color::srgb(0.2, 0.4, 0.8));
 
     commands.spawn((
         Mesh2d(rect_mesh),
         MeshMaterial2d(rect_material),
-        Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+        RigidBody::Fixed,
+        Collider::cuboid(60.0, 12.0),
+        Transform::from_translation(Vec3::new(0.0, -250.0, 0.0)),
+        Block,
+    )).insert(GravityScale(0.0));
+
+    let circle_mesh = CircleMeshBuilder::new(16.0, 64).build();
+
+    let mesh_handle = meshes.add(circle_mesh);
+
+    let circle_material = materials.add(Color::srgb(1.0, 0.0, 0.0));
+    commands.spawn((
+        Mesh2d(mesh_handle),
+        MeshMaterial2d(circle_material),
+        RigidBody::Dynamic,
+        Collider::ball(16.0),
+        Restitution::coefficient(0.7),
+        Transform::from_xyz(0.0, 400.0, 0.0),
     ));
+}
+
+fn setup_graphics(mut commands: Commands) {
+    // Add a camera so we can see the debug-render.
+    commands.spawn(Camera2d::default());
+}
+
+/*fn setup_physics(mut commands: Commands) {
+    /* Create the ground. */
+    commands
+        .spawn(Collider::cuboid(500.0, 50.0))
+        .insert(Transform::from_xyz(0.0, -100.0, 0.0));
+        
+} */
+
+fn print_ball_altitude(positions: Query<&Transform, With<RigidBody>>) {
+    for transform in positions.iter() {
+        println!("Ball altitude: {}", transform.translation.y);
+    }
+}
+
+#[derive(Component)]
+pub struct Block;
+
+fn move_player(
+    mut q_movement: Query<&mut Transform, With<Block>>,
+    input: ResMut<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+) {
+    for mut transform in &mut q_movement {
+        if input.pressed(KeyCode::KeyD) { transform.translation.x += 400.0 * time.delta_secs(); }
+        if input.pressed(KeyCode::KeyA) { transform.translation.x -= 400.0 * time.delta_secs(); }
+    }
 }
